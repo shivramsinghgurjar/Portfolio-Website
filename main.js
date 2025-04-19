b0a460898688ac5a984c82e20db6846f6dc801d4
shivramsinghgurjar <148748849+shivramsinghgurjar@users.noreply.github.com>
2025-04-20 03:26:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Fontend Developer", "Java Developer" ,"Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
